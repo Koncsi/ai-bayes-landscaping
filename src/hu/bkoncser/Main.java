988796d7be5b8d71a2ca31a3c0c7687f5babc13c
Koncser Bálint <bkoncser@atoll.local>
2016-10-26 12:01:29 +0200

    Main.java added

---
+++ src/hu/bkoncser/Main.java
@@ -0,0 +1,14 @@
+package hu.bkoncser;
+
+/**
+ * Created by bkoncser on 2016-10-26.
+ */
+public class Main {
+
+    public static void main(String[] args){
+
+
+        System.out.println("working...");
+    }
+
+}
